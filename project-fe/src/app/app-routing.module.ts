926a1e5f92f744b0b182ed7a1e1ff88575aad2b0
Jeff Lapp <lappjeff20@gmail.com>
2020-01-16 18:18:29 -0500

    Created routes for components

--- project-fe/src/app/app-routing.module.ts
+++ project-fe/src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { LoginComponent } from "./login/login.component";
+import { RegisterComponent } from "./register/register.component";
+import { UserhomeComponent } from "./userhome/userhome.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "login", pathMatch: "full" },
+  { path: "login", component: LoginComponent },
+  { path: "login", component: RegisterComponent },
+  { path: "login", component: UserhomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
